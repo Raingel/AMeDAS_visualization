9009dc66b5f01a2bc69079f8b037e98203e35c5f
OU JIEHAO <allenstorm2005@gmail.com>
2024-08-05 23:02:17 +0900

    Update AMeDAS_parser_recent.py

--- AMeDAS_parser_recent.py
+++ AMeDAS_parser_recent.py
@@ -37,7 +37,7 @@ def download_amedas_station_list():
     for file in os.listdir("ame_master"):
         if file.endswith(".csv"):
             AMeDAS_STA_file = file
-            AMeDAS_STA_df = pd.read_csv_with_multiple_encodings("ame_master/" + AMeDAS_STA_file)
+            AMeDAS_STA_df = read_csv_with_multiple_encodings("ame_master/" + AMeDAS_STA_file)
             break
 
     AMeDAS_STA_df["緯度"] = AMeDAS_STA_df.apply(lambda x: to_decimal(x["緯度(度)"], x["緯度(分)"]), axis=1)
